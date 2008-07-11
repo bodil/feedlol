a666c0a6d1f00e544c83fcc80687507aedae70fc
bodil <bodil@5bc59a62-733f-0410-a0db-ec011045df1d>
2008-07-11 16:56:12 +0000

    A nice timesince filter, thanks Django.
    git-svn-id: https://svn.bodil.tv/svn/trunk/feedlol@255 5bc59a62-733f-0410-a0db-ec011045df1d

--- src/feedlol/SiteServer.py
+++ src/feedlol/SiteServer.py
@@ -21,9 +21,7 @@ from feedlol.Notification import NotificationServer
 from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
 from routes import Mapper
-import cgi
-import os
-import re
+import cgi, os, re, time, datetime
 
 class QueryData(object):
     def __init__(self, query):
@@ -127,12 +125,67 @@ def filter_twitterise(value):
     value = twitterAtRe.sub(r'@<a href="http://twitter.com/\1">\1</a>', value)
     return value
 
+def timesince(d, now=None):
+    """
+    Takes two datetime objects and returns the time between d and now
+    as a nicely formatted string, e.g. "10 minutes".  If d occurs after now,
+    then "0 minutes" is returned.
+
+    Units used are years, months, weeks, days, hours, and minutes.
+    Seconds and microseconds are ignored.  Up to two adjacent units will be
+    displayed.  For example, "2 weeks, 3 days" and "1 year, 3 months" are
+    possible outputs, but "2 weeks, 3 hours" and "1 year, 5 days" are not.
+
+    Adapted from http://blog.natbat.co.uk/archive/2003/Jun/14/time_since
+    """
+    def ungettext(singular, plural, count):
+        return (count == 1) and singular or plural
+    chunks = (
+      (60 * 60 * 24 * 365, lambda n: ungettext('year', 'years', n)),
+      (60 * 60 * 24 * 30, lambda n: ungettext('month', 'months', n)),
+      (60 * 60 * 24 * 7, lambda n : ungettext('week', 'weeks', n)),
+      (60 * 60 * 24, lambda n : ungettext('day', 'days', n)),
+      (60 * 60, lambda n: ungettext('hour', 'hours', n)),
+      (60, lambda n: ungettext('minute', 'minutes', n))
+    )
+    # Convert datetime.date to datetime.datetime for comparison
+    if d.__class__ is not datetime.datetime:
+        d = datetime.datetime(d.year, d.month, d.day)
+    if now:
+        t = now.timetuple()
+    else:
+        t = time.gmtime()
+    now = datetime.datetime(t[0], t[1], t[2], t[3], t[4], t[5])
+
+    # ignore microsecond part of 'd' since we removed it from 'now'
+    delta = now - (d - datetime.timedelta(0, 0, d.microsecond))
+    since = delta.days * 24 * 60 * 60 + delta.seconds
+    if since <= 0:
+        # d is in the future compared to now, stop processing.
+        return u'0 minutes'
+    for i, (seconds, name) in enumerate(chunks):
+        count = since // seconds
+        if count != 0:
+            break
+    s = u'%(number)d %(type)s' % {'number': count, 'type': name(count)}
+    if i + 1 < len(chunks):
+        # Now get the second item
+        seconds2, name2 = chunks[i + 1]
+        count2 = (since - (seconds * count)) // seconds2
+        if count2 != 0:
+            s += u', %(number)d %(type)s' % {'number': count2, 'type': name2(count2)}
+    return s
+
+def filter_timesince(value):
+    return timesince(value)
+
 class SiteServer(QObject):
     def __init__(self, parent = None):
         QObject.__init__(self, parent)
         self.jinja = Environment(loader = FileSystemLoader("data/templates"), autoescape = True)
         self.jinja.filters["likes"] = filter_likes
         self.jinja.filters["twitterise"] = filter_twitterise
+        self.jinja.filters["timesince"] = filter_timesince
 
         self.mapper = Mapper()
         self.mapper.connect("/", controller = "LolViews", action = "feed")
